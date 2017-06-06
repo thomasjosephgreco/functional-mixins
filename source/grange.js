f9725ffbc14f394fdbb8cc3ec2975e9f05c05c04
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-06-06 01:18:09 -0400

    quick commit before implementing loop (begins with failing test)

---
+++ source/grange.js
@@ -0,0 +1,17 @@
+export function* grange (start, end, transform, options) {
+  const shouldTransform = typeof transform === 'function';
+
+  if (!shouldTransform) options = transform;
+  const { step = 1 } = options || {};
+
+  if (start > end) {
+    for (let i = start; i >= end; i -= step) {
+    yield shouldTransform ? transform(i) : i;
+  }
+    return;
+  }
+
+  for (let i = start; i <= end; i += step) {
+    yield shouldTransform ? transform(i) : i;
+  }
+}
---
+++ source/grange.test.js
@@ -0,0 +1,57 @@
+import test from 'tape';
+import { grange } from './grange';
+const range = (start, end) =>
+  Array.from({ length: end - start + 1 }, (x, i) => i++);
+
+test('simple number range', assert => {
+  const msg = 'should produce a certain range';
+
+  const [...actual] = grange(3, 6);
+  const expected = [ 3, 4, 5, 6];
+
+  assert.same(actual, expected, msg);
+  assert.end();
+});
+
+test('step', assert => {
+  const msg = 'should obey step increment';
+
+  const [...actual] = grange(2, 6, {step: 2});
+  const expected = [2, 4, 6];
+
+  assert.same(actual, expected, msg);
+  assert.end();
+});
+
+test('transform', assert => {
+  const msg = 'should use transform function if provided.';
+
+  const [...actual] = grange(1, 3, n => n * 2);
+  const expected = [2, 4, 6];
+
+  assert.same(actual, expected, msg);
+  assert.end();
+});
+
+test('descrememt', assert => {
+  const msg = 'should decrement when start > end.';
+
+  const [...actual] = grange(3, 1);
+  const expected = [3, 2, 1];
+
+  assert.same(actual, expected, msg);
+  assert.end();
+});
+
+test('loop', assert => {
+  const msg = 'should loop over range.';
+
+  const loopGen = grange(1, 3, {loop: true});
+  const actual = range(0, 7).map(() => loopGen.next().value);
+  const expected = [1, 2, 3, 1, 2, 3, 1, 2];
+
+  assert.same(actual, expected, msg);
+  assert.end();
+});
+
+
